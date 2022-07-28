6b004f6e529a0f5d46c6d24f2ade8d9e5816ef4e
조원영 <wycho@idevel.kr>
2022-07-28 23:59:11 +0900

    게시판css적용, 내용출력

--- app/Models/NoticeModel.php
+++ app/Models/NoticeModel.php
@@ -7,7 +7,7 @@ use CodeIgniter\Model;
 class NoticeModel extends Model
 {
     protected $table = 'notice';
-    protected $allowedFields = ['TITLE', 'CONTENT'];
+    protected $allowedFields = ['SEQ', 'TITLE', 'CONTENT', 'READ_CNT', 'SHOW_YN', 'DEL_YN', 'CREATED_BY', 'CREATED_AT'];
     protected $primaryKey = 'SEQ';
 
     protected $useTimestamps = true;
--- app/Views/post/index.php
+++ app/Views/post/index.php
@@ -8,11 +8,294 @@ function show_content($content) // (1)
     return $content;
 }
 ?>
-<ul>
-    <?php foreach ($post_list as $post) : ?>
-        <a href="<?= site_url("/post/show/{$post["SEQ"]}") ?>"><?= $post["TITLE"] ?></a>  <!-- (5) -->
-        <div><?= show_content($post["CONTENT"]) ?></div>
-        <a href="<?= site_url("/post/show/{$post["SEQ"]}/#CONTENT") ?>">Read more</a>
-    <?php endforeach ?>
-</ul>
-<?= $pager->links() ?> <!-- (6) -->
+<!DOCTYPE html>
+<html lang="ko">
+<style>
+    table {
+        border-collapse: collapse;
+        border-spacing: 0;
+    }
+    section.notice {
+        padding: 80px 0;
+    }
+
+    .page-title {
+        margin-bottom: 60px;
+    }
+    .page-title h3 {
+        font-size: 28px;
+        color: #333333;
+        font-weight: 400;
+        text-align: center;
+    }
+
+    #board-search .search-window {
+        padding: 15px 0;
+        background-color: #f9f7f9;
+    }
+    #board-search .search-window .search-wrap {
+        position: relative;
+        /*   padding-right: 124px; */
+        margin: 0 auto;
+        width: 80%;
+        max-width: 564px;
+    }
+    #board-search .search-window .search-wrap input {
+        height: 40px;
+        width: 100%;
+        font-size: 14px;
+        padding: 7px 14px;
+        border: 1px solid #ccc;
+    }
+    #board-search .search-window .search-wrap input:focus {
+        border-color: #333;
+        outline: 0;
+        border-width: 1px;
+    }
+    #board-search .search-window .search-wrap .btn {
+        position: absolute;
+        right: 0;
+        top: 0;
+        bottom: 0;
+        width: 108px;
+        padding: 0;
+        font-size: 16px;
+    }
+
+    .board-table {
+        font-size: 13px;
+        width: 100%;
+        border-top: 1px solid #ccc;
+        border-bottom: 1px solid #ccc;
+    }
+
+    .board-table a {
+        color: #333;
+        display: inline-block;
+        line-height: 1.4;
+        word-break: break-all;
+        vertical-align: middle;
+    }
+    .board-table a:hover {
+        text-decoration: underline;
+    }
+    .board-table th {
+        text-align: center;
+    }
+
+    .board-table .th-num {
+        width: 100px;
+        text-align: center;
+    }
+
+    .board-table .th-date {
+        width: 200px;
+    }
+
+    .board-table th, .board-table td {
+        padding: 14px 0;
+    }
+
+    .board-table tbody td {
+        border-top: 1px solid #e7e7e7;
+        text-align: center;
+    }
+
+    .board-table tbody th {
+        padding-left: 28px;
+        padding-right: 14px;
+        border-top: 1px solid #e7e7e7;
+        text-align: left;
+    }
+
+    .board-table tbody th p{
+        display: none;
+    }
+
+    .btn {
+        display: inline-block;
+        padding: 0 30px;
+        font-size: 15px;
+        font-weight: 400;
+        background: transparent;
+        text-align: center;
+        white-space: nowrap;
+        vertical-align: middle;
+        -ms-touch-action: manipulation;
+        touch-action: manipulation;
+        cursor: pointer;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+        border: 1px solid transparent;
+        text-transform: uppercase;
+        -webkit-border-radius: 0;
+        -moz-border-radius: 0;
+        border-radius: 0;
+        -webkit-transition: all 0.3s;
+        -moz-transition: all 0.3s;
+        -ms-transition: all 0.3s;
+        -o-transition: all 0.3s;
+        transition: all 0.3s;
+    }
+
+    .btn-dark {
+        background: #555;
+        color: #fff;
+    }
+
+    .btn-dark:hover, .btn-dark:focus {
+        background: #373737;
+        border-color: #373737;
+        color: #fff;
+    }
+
+    .btn-dark {
+        background: #555;
+        color: #fff;
+    }
+
+    .btn-dark:hover, .btn-dark:focus {
+        background: #373737;
+        border-color: #373737;
+        color: #fff;
+    }
+
+    /* reset */
+
+    * {
+        list-style: none;
+        text-decoration: none;
+        padding: 0;
+        margin: 0;
+        box-sizing: border-box;
+    }
+    .clearfix:after {
+        content: '';
+        display: block;
+        clear: both;
+    }
+    .container {
+        width: 1100px;
+        margin: 0 auto;
+    }
+    .blind {
+        position: absolute;
+        overflow: hidden;
+        clip: rect(0 0 0 0);
+        margin: -1px;
+        width: 1px;
+        height: 1px;
+    }
+    .page_wrap {
+        text-align:center;
+        font-size:0;
+    }
+    .page_nation {
+        display:inline-block;
+
+    }
+    .page_nation .none {
+        display:none;
+    }
+    .page_nation a {
+        display:block;
+        margin:0 3px;
+        float:left;
+        border:1px solid #e6e6e6;
+        width:28px;
+        height:28px;
+        line-height:28px;
+        text-align:center;
+        background-color:#fff;
+        font-size:13px;
+        color:#999999;
+        text-decoration:none;
+    }
+    .page_nation .arrow {
+        border:1px solid #ccc;
+    }
+    .page_nation .pprev {
+        background:#f8f8f8 url('/img/page_pprev.png') no-repeat center center;
+        margin-left:0;
+    }
+    .page_nation .prev {
+        background:#f8f8f8 url('img/page_prev.png') no-repeat center center;
+        margin-right:7px;
+    }
+    .page_nation .next {
+        background:#f8f8f8 url('img/page_next.png') no-repeat center center;
+        margin-left:7px;
+    }
+    .page_nation .nnext {
+        background:#f8f8f8 url('img/page_nnext.png') no-repeat center center;
+        margin-right:0;
+    }
+    .page_nation a.active {
+        background-color:#42454c;
+        color:#fff;
+        border:1px solid #42454c;
+    }
+</style>
+<body>
+<section class="notice">
+    <div class="page-title">
+        <div class="container">
+            <h3>공지사항</h3>
+        </div>
+    </div>
+
+    <!-- board seach area -->
+    <div id="board-search">
+        <div class="container">
+            <div class="search-window">
+                <form action="">
+                    <div class="search-wrap">
+                        <label for="search" class="blind">공지사항 내용 검색</label>
+                        <input id="search" type="search" name="" placeholder="검색어를 입력해주세요." value="">
+                        <button type="submit" class="btn btn-dark">검색</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    <!-- board list area -->
+    <div id="board-list">
+        <div class="container">
+            <table class="board-table">
+                <thead>
+                <tr>
+                    <th scope="col" class="th-num">번호</th>
+                    <th scope="col" class="th-title">제목</th>
+                    <th scope="col" class="th-date">등록일</th>
+                </tr>
+                </thead>
+                <tbody>
+                <?php
+                $no = 0;
+                foreach ($post_list as $post):
+                $no ++;
+                $post['SEQ'];
+                echo <<<TR
+                <tr>
+                    <td>{$no}</td>
+                    <td><a href="/show.php?seq={$post['SEQ']}">{$post['TITLE']}</a></td>
+                    <td>{$post['CREATED_AT']}</td>
+                </tr>
+                TR;
+                endforeach;
+                ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</body>
+</section>
+<div class="page_wrap">
+    <div class="page_nation">
+        <?= $pager->links() ?>
+    </div>
+</div>
+
+</html>
